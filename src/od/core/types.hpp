dd37f22503b23c654538bb5d6b3ae0abcc953719
Mika Tammi <mika@Mika-Tammis-MacBook.local>
2012-07-28 23:15:04 +0300

    Added core type definitions

---
+++ src/od/core/types.hpp
@@ -0,0 +1,16 @@
+#ifndef OD_CORE_TYPES_HPP
+#define OD_CORE_TYPES_HPP
+
+#include <stdint.h>
+
+typedef uint8_t ODuint8;
+typedef uint16_t ODuint16;
+typedef uint32_t ODuint32;
+typedef uint64_t ODuint64;
+
+typedef int8_t ODint8;
+typedef int16_t ODint16;
+typedef int32_t ODint32;
+typedef int64_t ODint64;
+
+#endif /* OD_CORE_TYPES_HPP */
