e2ccc14fe85c9544b75ac173fa7a2110bf297881
Mika Tammi <mikatammi@gmail.com>
2012-07-30 01:00:54 +0300

    Made OpenGL.hpp include OpenGL headers only in case of OpenGL present

--- src/od/graphics/hal/gl/OpenGL.hpp
+++ src/od/graphics/hal/gl/OpenGL.hpp
@@ -1,8 +1,10 @@
 #ifndef OD_GRAPHICS_HAL_GL_OPENGL_HPP
 #define OD_GRAPHICS_HAL_GL_OPENGL_HPP
 
+#ifdef OPENGL
 #ifdef GLEW
 #include <glew.h>
-#endif
+#endif /* GLEW */
+#endif /* OPENGL */
 
 #endif /* OD_GRAPHICS_HAL_GL_OPENGL_HPP */
