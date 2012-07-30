19b87a4e0b3126b13b7746df06f3ec29da49e7e3
Mika Tammi <mikatammi@gmail.com>
2012-07-30 17:18:38 +0300

    Added Window creation to example program

--- examples/hal_window/main.cpp
+++ examples/hal_window/main.cpp
@@ -1,12 +1,17 @@
 #include <cstdlib>
 #include <od/graphics/hal/DeviceFactory.hpp>
 #include <od/graphics/hal/Device.hpp>
+#include <od/graphics/hal/Window.hpp>
 
 using od::graphics::hal::DeviceFactory;
 using od::graphics::hal::Device;
+using od::graphics::hal::Window;
 
 int main()
 {
     Device* device = DeviceFactory::createDevice();
+
+    Window* window = device->createWindow();
+
     return EXIT_SUCCESS;
 }
