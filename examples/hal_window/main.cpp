bfe50fc87360d03ed34e4f6bc2bc4ba0a9ee81db
Mika Tammi <mikatammi@gmail.com>
2012-08-01 01:31:13 +0300

    HAL Window example uses Qt Window for now

--- examples/hal_window/main.cpp
+++ examples/hal_window/main.cpp
@@ -2,16 +2,45 @@
 #include <od/graphics/hal/DeviceFactory.hpp>
 #include <od/graphics/hal/Device.hpp>
 #include <od/graphics/hal/Window.hpp>
+#include <od/graphics/hal/gl/GLQtWindow.hpp>
+#include <QApplication>
 
 using od::graphics::hal::DeviceFactory;
 using od::graphics::hal::Device;
 using od::graphics::hal::Window;
+using od::graphics::hal::gl::GLQtWindow;
 
-int main()
+int main(int argc, char** argv)
 {
-    Device* device = DeviceFactory::createDevice();
+    // Initializing Qt Application
+    QApplication app(argc, argv);
 
-    Window* window = device->createWindow();
+    // Create new ODlib HAL Device
+    Device* device = DeviceFactory::createDevice(
+                od::graphics::hal::DEVICETYPE_OPENGLES2);
+
+    // If HAL Device could not be created, exit
+    if(!device)
+    {
+        return EXIT_FAILURE;
+    }
+
+    // Create new GL Qt Window
+    GLQtWindow* window = new GLQtWindow();
+
+    // If window can't be set, exit
+    if(!device->setWindow(window))
+    {
+        delete device;
+        delete window;
+
+        return EXIT_FAILURE;
+    }
+
+    app.exec();
+
+    delete device;
+    delete window;
 
     return EXIT_SUCCESS;
 }
