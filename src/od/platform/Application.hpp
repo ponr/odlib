23792a9cba9492e1a2a1ab95c45bd03928b06588
Mika Tammi <mikatammi@gmail.com>
2012-08-03 04:35:04 +0300

    Moved Window handling functions under Application class

--- src/od/platform/Application.hpp
+++ src/od/platform/Application.hpp
@@ -4,11 +4,26 @@
 namespace od {
 namespace platform {
 
+class Window;
+
 class Application
 {
 public:
     /// Destructor
     virtual ~Application();
+
+    /// Create window
+    /// @return Pointer to window
+    virtual Window* createWindow() = 0;
+
+    /// Set Device to use given Window
+    /// @param window Pointer to window
+    /// @return Bool to tell if set succeeded or not
+    virtual bool setWindow(Window* window) = 0;
+
+    /// Get Window of the Device
+    /// @return Pointer to window
+    virtual Window* getWindow() = 0;
 
 protected:
     Application(int argc, char ** argv);
