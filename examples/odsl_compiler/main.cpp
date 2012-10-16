45689b86e9c9e09752955e6d6352b7bff2640779
Mika Tammi <mikatammi@gmail.com>
2012-10-16 07:17:43 +0300

    Made odsl_compiler example file reading to string more elegant.

--- examples/odsl_compiler/main.cpp
+++ examples/odsl_compiler/main.cpp
@@ -10,44 +10,40 @@ int main(int argc, char** argv)
     using std::cerr;
     using std::endl;
     using std::ifstream;
+    using std::istreambuf_iterator;
     using std::string;
     using od::graphics::odsl::ShaderCompiler;
 
-    ShaderCompiler compiler;
-
+    // Check parameter count
     if(argc < 2)
     {
         cerr << "Error, no parameter given" << endl;
         return EXIT_FAILURE;
     }
 
+    // Open file given in parameter
     ifstream inputfile;
     inputfile.open(argv[1]);
 
+    // Check if opened
     if(!inputfile.is_open())
     {
         cerr << "Error, file '" << argv[1] << "' could not be opened" << endl;
         return EXIT_FAILURE;
     }
 
-    inputfile.seekg(0, std::ios::end);
-    int length = inputfile.tellg();
-    inputfile.seekg(0, std::ios::beg);
+    // Don't skip white space
+    inputfile.unsetf(std::ios::skipws);
 
-    char * buf = new char[length];
-    inputfile.read(buf, length);
-
-    string shadersource(buf, 0, length);
-
-    delete[] buf;
-
+    // Read file to string using istreambuf iterators
+    string shadersource(istreambuf_iterator <char> (inputfile.rdbuf()),
+                        istreambuf_iterator <char> () );
     inputfile.close();
 
-    // Compiler shader source read from file
+    // Compile shader source read from file
+    ShaderCompiler compiler;
     compiler.setSource(shadersource);
     compiler.compile();
-    //cout << compiler.getCompilationError() << endl;
-
 
     return EXIT_SUCCESS;
 }
